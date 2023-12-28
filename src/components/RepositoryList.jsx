65c7cea2fc88f3ea5e80483e4bd2390eff4a2f1c
Emanuel Acosta <e.acosta1695@gmail.com>
2023-12-28 15:25:15 -0300

    Add function for fetch 'Rate Repository API' backend.

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,8 +1,26 @@
 import { Text, FlatList } from "react-native"
-import repositories from "../data/repositories.js"  // Fix the typo here
+// import repositories from "../data/repositories.js"  // Fix the typo here
 import RepositoryItem from "./RepositoryItem.jsx"
+import React, { useEffect, useState } from "react"
 
 const RepositoryList = () => {
+    const [repositories, setRepositories] = useState(null)
+    
+
+    const fetchRepositories = async () => {
+        const response = await global.fetch('http://localhost:5000/api/repositories');
+        const json = await response.json();
+        setRepositories(json);
+    }
+
+    useEffect(() => {
+        fetchRepositories()
+    }, [])
+
+    const repositoriesNodes = repositories ?
+        repositories.edges.map(edge => edge.node)
+        : []
+
     return (
         // data={repositories} = la data que tiene que renderizar
         // renderItem={} = metodo de como tiene que renderizar cada item
